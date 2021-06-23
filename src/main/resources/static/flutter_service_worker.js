f5e840d40a6d8d5206d811175fa89f05ae119a9a
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-06-23 15:48:49 -0500

    back end terminado

--- src/main/resources/static/flutter_service_worker.js
+++ src/main/resources/static/flutter_service_worker.js
@@ -3,14 +3,38 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "58ee5163cbed2ba04f09e33ebc1ac3a5",
+  "assets/AssetManifest.json": "ab2091b91951c090958e7171308a3f0c",
 "assets/assets/fonts/Itim-Regular.ttf": "ea9ed37af3e95dd1629a9581ceb2c4ed",
 "assets/assets/fonts/Montserrat-Medium.ttf": "c8b6e083af3f94009801989c3739425e",
 "assets/assets/fonts/Pacifico-Regular.ttf": "9b94499ccea3bd82b24cb210733c4b5e",
+"assets/assets/images/avatar_profile.png": "76b7cc338e6fcd248583ef0a93d6719d",
+"assets/assets/images/baby-beef.png": "0840f2d8511bbb7cdae9cba9c92dc7db",
+"assets/assets/images/bell2.png": "f7724ce530e6cd09df8695fa83889276",
+"assets/assets/images/cover_page.jpg": "ed0251383a6cd14b44473176da8bd5a0",
+"assets/assets/images/cutlery.png": "14a8013ab033223c839d34438292f9b7",
+"assets/assets/images/diet.png": "e52894a42739b9aa743a10f8dd7801c7",
+"assets/assets/images/FamiliarGrande.png": "9643425d329a1e2a593d0d3668e11c2a",
+"assets/assets/images/farmacia.png": "677172d6cebfed4101a3906446050be2",
+"assets/assets/images/fast-food.png": "c91fbe03c6133bb7294c7f948e81624b",
+"assets/assets/images/ferreteria.png": "863d4380a04db7498c58b54dfcda51ba",
+"assets/assets/images/hamburguesa_ejemplo.jpg": "a66b44ebc0e4c4849cad43579acf8ec9",
+"assets/assets/images/hotdog.jpg": "ce3a8bf17b517b15a9ebe946fe3b8e1b",
+"assets/assets/images/loader.gif": "44808102085ce802d283b9dddae53074",
 "assets/assets/images/login_background.png": "91b0ab8639bb7c57ad9655c4ea2f55bc",
+"assets/assets/images/logoapp.png": "3b60a88a3b79391cbd630e0b037e1ffc",
+"assets/assets/images/mascotas.png": "959558aa77f98576b47949e7c6681ab3",
+"assets/assets/images/medicine.png": "383a85d24b3e67c230a8dad4ba53f61a",
+"assets/assets/images/papeleria.png": "9a013a93a02607dbcb34a874e371f840",
+"assets/assets/images/profile_picture.png": "b214b8dcda002fbec06851badbfefed2",
+"assets/assets/images/profile_picture2.png": "9eba71abe34d4022bb97b696f8bb9aeb",
+"assets/assets/images/restaurant.png": "6fa4d24331ce925bb1234cd3b5677c3c",
+"assets/assets/images/settings.png": "59fcb6d90a7e20297b171a2347984e0b",
+"assets/assets/images/shopping-cart.png": "fef92a65de967b9cb19bc3509cff26f6",
+"assets/assets/images/stonks.jpg": "de09716dc10e05683e929c0626a61e9d",
+"assets/assets/images/user.png": "7d56d975418a52a571ae93e9f64254cd",
 "assets/FontManifest.json": "36a91115fb532d70a55f941e96aaa843",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "0473d50334a9b458a2383597125ab5d2",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "fc755b930f5fc3c4aa8b88de483c8fc7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/flutter_signin_button/assets/logos/2.0x/facebook_new.png": "83bf0093719b2db2b16e2839aee1069f",
 "assets/packages/flutter_signin_button/assets/logos/2.0x/google_dark.png": "937022ea241c167c8ce463d2ef7ed105",
@@ -27,9 +51,9 @@ const RESOURCES = {
 "favicon.png": "520d34bcf694ac66a14c7252af16c2dd",
 "icons/Icon-192.png": "3d412df74eb1a529a35d9af1cc43f88a",
 "icons/Icon-512.png": "fd350e879b23f10e03c1206af189c0b9",
-"index.html": "dc17d2b605deec3041a7860e42f20299",
-"/": "dc17d2b605deec3041a7860e42f20299",
-"main.dart.js": "627b3a195bcb49aedcc29e450d53dfa5",
+"index.html": "263729feae1a6ba2f7bbd19bed9ebe0a",
+"/": "263729feae1a6ba2f7bbd19bed9ebe0a",
+"main.dart.js": "3398b5f16287166e75b19926e6a64e0d",
 "manifest.json": "11f0001a5434417004e393ef72920ecc",
 "version.json": "6fe16395189bb09502d44fc18e122c77"
 };
@@ -49,7 +73,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
